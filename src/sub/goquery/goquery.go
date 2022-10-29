418d91c4bd5e7bef116f5d6b0f79911c7a5d8646
user_komeda <shigoto922@gmail.com>
2022-10-29 23:07:55 +0900

    #3 カテゴリに紐づくコンテンツの取得

--- src/sub/goquery/goquery.go
+++ src/sub/goquery/goquery.go
@@ -3,16 +3,53 @@ package goquery
 import (
 	"fmt"
 	"strings"
+	"time"
+	"user-komeda/GOLANG_SCRAIPE/src/sub/selenium"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/sclevine/agouti"
 )
 
-func Goquery(html string) {
-	htmlContentReader := strings.NewReader(html)
-	htmlContent, _ := goquery.NewDocumentFromReader(htmlContentReader)
-	content_a := htmlContent.Find(".cw-nav .parent .cw-list_nav_subcategory_title ")
-	content_a.Each(func(i int, s *goquery.Selection) {
+func GoqueryForCategory(html string) {
+	htmlContent := goqueryBase(html)
+	contentA := htmlContent.Find(".cw-nav .parent .cw-list_nav_subcategory_title ")
+	contentA.Each(func(i int, s *goquery.Selection) {
 		fmt.Println(s.Text())
 		fmt.Println(s.Parent().Find("ul").Text())
 	})
+}
+
+func GoqueryForMainContent(html string, page *agouti.Page) {
+	urlList := getCategoryURL(html)
+	for _, url := range urlList {
+		fmt.Println(url)
+		html, _ := selenium.NavigatePage(url, page)
+		htmlContent := goqueryBase(html)
+		title := htmlContent.Find(".item_title")
+		title.Each(func(i int, s *goquery.Selection) {
+			fmt.Println(s.Text())
+			fmt.Println("-------")
+			time.Sleep(time.Second * 1)
+		})
+		fmt.Println("=======")
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func goqueryBase(html string) *goquery.Document {
+
+	htmlContentReader := strings.NewReader(html)
+	htmlContent, _ := goquery.NewDocumentFromReader(htmlContentReader)
+
+	return htmlContent
+}
+
+func getCategoryURL(html string) []string {
+	htmlContent := goqueryBase(html)
+	categoryURL := htmlContent.Find(".cw-nav .parent .cw-list_nav_subcategory_title").Children()
+	urlList := []string{}
+	categoryURL.Each(func(i int, s *goquery.Selection) {
+		urlList = append(urlList, s.AttrOr("href", ""))
+	})
+	return urlList
 }
--- src/sub/selenium/selenium.go
+++ src/sub/selenium/selenium.go
@@ -7,26 +7,32 @@ import (
 	"github.com/sclevine/agouti"
 )
 
-func Selenium() (string, error) {
+func Selenium() (string, error, *agouti.Page, *agouti.WebDriver) {
+	const url string = "/public/jobs?category=jobs&order=score"
 	driver := agouti.ChromeDriver(agouti.Browser("chrome"))
-	defer driver.Stop()
+	// defer driver.Stop()
 
 	if err := driver.Start(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return err.Error(), err
+		return err.Error(), err, nil, nil
 	}
 
 	page, err := driver.NewPage()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
-		return err.Error(), err
+		return err.Error(), err, nil, nil
 	}
+	html, err := NavigatePage(url, page)
+	return html, err, page, driver
+}
 
+func NavigatePage(url string, page *agouti.Page) (string, error) {
 	// googleにアクセス
-	if err := page.Navigate("https://crowdworks.jp/public/jobs?category=jobs&order=score"); err != nil {
+	if err := page.Navigate("https://crowdworks.jp" + url); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		return err.Error(), err
 	}
 
 	return page.HTML()
+
 }
